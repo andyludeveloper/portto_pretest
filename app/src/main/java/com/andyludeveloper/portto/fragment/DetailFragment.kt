d5cd0f0fe2579e8f5aa137565ba3f37e256f4ec9
Andy <andyludeveloper@gmail.com>
2022-03-09 00:56:35 +0800

    implement detail fragment with permalink button

--- app/src/main/java/com/andyludeveloper/portto/fragment/DetailFragment.kt
+++ app/src/main/java/com/andyludeveloper/portto/fragment/DetailFragment.kt
@@ -1,18 +1,19 @@
 package com.andyludeveloper.portto.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.andyludeveloper.portto.R
 import com.andyludeveloper.portto.viewmodel.CollectionViewModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
+
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
     private val collectionViewModel: CollectionViewModel by activityViewModels()
@@ -35,11 +36,7 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             requestManager.load(asset.image_url).into(imageView)
             descriptionView.text = asset.collection.description
             permalinkBtn.setOnClickListener {
-                Toast.makeText(
-                    requireContext(),
-                    asset.collection.description,
-                    Toast.LENGTH_SHORT
-                ).show()
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(asset.permalink)))
             }
         }
     }
